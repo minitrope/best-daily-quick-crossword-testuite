bd2b0d3371ce0f53002a51bbb3fe24ba3ff3b444
Romain Gauthier <romain.gauthier@monkeypatch.me>
2023-09-07 08:02:58 +0200

    Add firefox to the tested platforms.

--- playwright.config.js
+++ playwright.config.js
@@ -16,10 +16,10 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ]
 });
 
